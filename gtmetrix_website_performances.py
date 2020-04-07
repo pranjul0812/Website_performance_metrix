8b5d2671a2f5503a631733349d98ed0f8c91ba4d
Pranjul Mishra <MTK06013@apj.mediatek.inc>
2020-04-07 12:55:05 +0530

    updating .py

--- gtmetrix_website_performances.py
+++ gtmetrix_website_performances.py
@@ -1,3 +1,11 @@
+'''Test Scenario: Check the website speed of all the website URLs listed in excel sheet. Results need to be shown in front of each website respectively with proper labeling clarifying the output.
+Speed needs to be checked on -
+i) URL: https://gtmetrix.com/ (For this, both grade and time need to be captured)
+
+Also, mark the grades/time with red if:
+i) grade is other than 'A' or 'B'.
+ii) time is greater than 4 seconds'''
+
 import pandas as pd
 import time
 import os
@@ -6,12 +14,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import *
+import openpyxl
+from openpyxl.styles import Font, colors
 
 
 class WebsitePerformanceCalculator:
 
     def read_excel(self):
-        df = pd.read_excel("C:\\Users\\Pranjul Mishra\\Desktop\\URL_list.xlsx")
+        df = pd.read_excel("URL_list.xlsx")
         df.drop(df.columns[0], axis=1, inplace=True)
         header = df.iloc[1]
         new_df = pd.DataFrame(df.values[2:], columns=header)
@@ -23,6 +33,7 @@ class WebsitePerformanceCalculator:
         else:
             filename = "GtMetrix_website_performances.xlsx"
         df.to_excel(filename, index=False, header=True)
+        self.result_update(filename)
 
     def gtmetrix(self, driver, df, wait):
         gt_output_time = []
@@ -49,26 +60,48 @@ class WebsitePerformanceCalculator:
                                                        "//div[@class='report-page-detail'][1]//span[@class='report-page-detail-value']")
                     time_value = time_element.text
                     gt_output_time.append(time_value)
-                    print(grade[-1], " ", time_value)
                     driver.back()
                     time.sleep(2)
                 except TimeoutException:
                     gt_output_time.append("ERROR")
                     gt_output_grade.append("ERROR")
-                    print("Error", " ", "Error")
                     driver.back()
             else:
                 gt_output_time.append("0")
                 gt_output_grade.append("0")
-                print("No Grade", "No Time value")
         df['gt_metrix_grade'] = gt_output_grade
         df['gt_metrix_time'] = gt_output_time
-
         self.write_excel(df)
+
+    def result_update(self, filename):
+        wb = openpyxl.load_workbook(filename)
+        ws = wb.active
+        max_row = ws.max_row
+        ft = Font(color=colors.RED)
+        valid_grade = ['A', 'B']
+        values = ["ERROR", "0"]
+        for row in range(2, (max_row + 1)):
+            cell2 = "B{}".format(row)
+            cell3 = "C{}".format(row)
+            wc2 = ws.cell(row, 2)
+            wc3 = ws.cell(row, 3)
+            grade = ws[cell2].value
+            time1 = ws[cell3].value
+            if grade in values:
+                wc2.font = ft
+                wc3.font = ft
+            else:
+                if grade not in valid_grade:
+                    wc2.font = ft
+                if "." in time1:
+                    new_time = float(time1[0:-1])
+                    if new_time > 4.0:
+                        wc3.font = ft
+        wb.save(filename)
 
 
 sa = WebsitePerformanceCalculator()
-driverLocation = "C:\\Pranjul\\webdriver\\chromedriver.exe"
+driverLocation = "C:\\Pranjul\\webdriver\\chromedriver.exe"  # as per your webderiver location
 os.environ['webdriver.driver.chrome'] = driverLocation
 driver = webdriver.Chrome(driverLocation)
 wait = WebDriverWait(driver, 120, poll_frequency=2)
